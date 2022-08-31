398cce020371008638c459bd491d34865c1e0ad4
Shade <form3pelican@gmail.com>
2022-08-31 21:35:22 +0400

    Initial Commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   key: '2bbdbf8b9187727b7250d3770206138a',
-  baseUrl: 'https://shadeflixservice.ga/api/v1',
+  baseUrl: 'https//97.108.210.207:8090/api/v1/',
   production: true
 };
